42e0e359a5a0ccf3dac53655bb0b1357797f5a14
刘港欢 <admin@arloor.com>
2023-12-23 01:06:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,1 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-host x86_64-unknown-linux-gnu -y
-cat <<\EOF |tee /etc/profile.d/rust.sh
-source "$HOME/.cargo/env"
-EOF
